ed2bf105504380c3e887ae181a62de054e6c13c6
Markus Hens <markus.hens@serie-a.de>
2019-01-07 17:51:05 +0100

    Created infrastructure for spring boot starter

--- event-producer-spring-boot-autoconfigure/src/main/java/de/mhens/events/autoconfigure/EventProperties.java
+++ event-producer-spring-boot-autoconfigure/src/main/java/de/mhens/events/autoconfigure/EventProperties.java
@@ -2,7 +2,7 @@ package de.mhens.events.autoconfigure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "mhens.event")
+@ConfigurationProperties(prefix = "de.mhens.eventproducer")
 public class EventProperties {
 
 }
